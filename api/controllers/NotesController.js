418163b8570ba5987a054f5b8af4eb28d77d4c01
Jake Schievink <jakeschievink@gmail.com>
2014-02-16 07:15:18 -0500

    fixed some lost some

--- api/controllers/NotesController.js
+++ api/controllers/NotesController.js
@@ -19,7 +19,7 @@ module.exports = {
 
 	index:function(req,res) {
 
-	}
+	},
 
 
   /**
--- assets/js/loader.js
+++ assets/js/loader.js
@@ -2,12 +2,18 @@ console.log(patient_id);
 console.log("dafadsfadsfa");
 var notesContainer = $("#viewnotes");
 var historyContainer = $("#history");
-var labsContainer = $("#labs");
+var labsContainer = $("#viewlabs");
 var perscriptionsContainer = $("#perscriptions");
 $.get("/notes?patient_id="+patient_id, function(notes){
     notes.forEach(function(note) {
         notesContainer.prepend("Created at:" + note.createdAt + "<br>"+ note.note);
     });
 
+
+});
+$.get("/labs?patient_id="+patient_id, function(labs){
+    labs.forEach(function(lab) {
+        labsContainer.prepend("Created at:" + lab.createdAt + "<br>"+ lab.lab);
+    });
 
 });
